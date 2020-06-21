f15f4178cfe7dcc9a99673347867505f8b450667
edgineer <elopez42@ucla.edu>
2020-06-21 00:43:29 -0700

    basic react setup chrome extension

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return <h1>Let's get started</h1>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
