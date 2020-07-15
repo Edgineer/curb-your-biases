3aa6a2f7a7e225753bc6f983952ac4eddaacda5b
edgineer <elopez42@ucla.edu>
2020-07-14 20:13:02 -0700

    resolved growing state error and renaming isssue

--- src/session.js
+++ src/session.js
@@ -8,27 +8,26 @@ class Session extends React.Component {
     super(props);
     this.state= { 
       numWords: 0,
-      words: [],
-      hover: []
+      randWords: [],
+      isClicked: []
     }
     this.reset = this.reset.bind(this);
-    this.mouseChange = this.mouseChange.bind(this);
+    this.mouseClick = this.mouseClick.bind(this);
   }
 
   reset () {
-    var newNum = Math.floor(Math.random() * (5 - 2)) + 2;
-    var initHover = Array(newNum).fill(false);
+    var num = Math.floor(Math.random() * (5 - 2)) + 2;
+    var initHover = Array(num).fill(false);
     var newWords = [];
-    for (let index = 0; index < newNum; index++) {
-      var randomWord = frenchWords[Math.floor(Math.random()*frenchWords.length)];
-      newWords.push({"word": randomWord.word, "translation": randomWord.translation});
+    for (let i = 0; i < num; i++) {
+      var randWord = frenchWords[Math.floor(Math.random()*frenchWords.length)];
+      newWords.push({"word": randWord.word, "translation": randWord.translation});
     }
-    console.log(newWords);
     this.setState(() => {
       return { 
-        numWords: newNum,
-        words: [...this.state.words, ...newWords],
-        hover: [...this.state.hover, ...initHover]
+        numWords: num,
+        randWords: [...newWords],
+        isClicked: [...initHover]
       }
     });
   }
@@ -37,22 +36,22 @@ class Session extends React.Component {
     this.reset();
   }
 
-  mouseChange(index) {
+  mouseClick(index) {
     this.setState((state) => {
-      const hover = state.hover.map((item,i) => {
+      const isClicked = state.isClicked.map((item,i) => {
         if (index === i) {return !item;}
         else {return item;}
       });
-      console.log(this.state.hover);
-      return {hover};
+      console.log(this.state.isClicked);
+      return {isClicked};
     });
   }
 
   render() {
-    const wordList = this.state.words.map((word, index) =>(
+    const randWordList = this.state.randWords.map((randWord, i) =>(
       <li>
-        <button onClick={() => this.mouseChange(index)}>
-          {this.state.hover[index] ? word.translation : word.word}
+        <button onClick={() => this.mouseClick(i)}>
+          {this.state.isClicked[i] ? randWord.translation : randWord.word}
         </button>
       </li>
     ));
@@ -60,7 +59,7 @@ class Session extends React.Component {
     <>
       <h1>Mumbo Jumbo!</h1>
       <p> Create a sentence that contains the following words:</p>
-      <ul>{wordList}</ul>
+      <ul>{randWordList}</ul>
       <textarea placeholder="Be creative!" />
       <button id="submit" onClick={this.reset}>Submit!</button>
       <hr />
