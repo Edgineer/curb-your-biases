38a02ed6db7040a1ee377698a531b6fa731c33c1
edgineer <elopez42@ucla.edu>
2020-07-16 14:01:27 -0700

    updates to package.json

--- src/session.js
+++ src/session.js
@@ -1,7 +1,10 @@
 import React from "react";
 import './index.css';
+import axios from "axios";
+import "regenerator-runtime/runtime.js";
 
 var frenchWords = require("../french.json");
+
 
 class Session extends React.Component {
   constructor(props){
@@ -11,7 +14,9 @@ class Session extends React.Component {
       randWords: [],
       isClicked: [],
       userInput: "",
-      charCount: 0
+      charCount: 0,
+      hasSubmitted: false,
+      errors: []
     }
     this.reset = this.reset.bind(this);
     this.mouseClick = this.mouseClick.bind(this);
@@ -33,7 +38,9 @@ class Session extends React.Component {
         randWords: [...newWords],
         isClicked: [...initHover],
         userInput: "",
-        charCount: 0
+        charCount: 0,
+        hasSubmitted: false,
+        errors: []
       }
     });
   }
@@ -48,23 +55,58 @@ class Session extends React.Component {
         if (index === i) {return !item;}
         else {return item;}
       });
-      console.log(this.state.isClicked);
       return {isClicked};
     });
   }
 
   userTyping(event){
     var input = event.target.value;
-    console.log(input);
-    console.log(input.length);
     this.setState({
       userInput: input,
       charCount: input.length
     });
   }
 
-  checkInput() {
-    this.reset();  
+  async checkInput() {
+    var payload = new URLSearchParams();
+    payload.set("text",this.state.userInput);
+    payload.set("language","fr");
+    payload.set("enabledOnly","false");
+    
+    var res = await fetch("https://languagetool.org/api/v2/check",{
+      method: 'POST',
+      body: payload.toString(),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+    if(!res.ok){
+      console.log("Sorry there is an error!");
+    }
+    var data = await res.json();
+    console.log(data);
+
+    var errList = [];
+    data.matches.forEach(error => {
+      var errObj = {};
+      errObj["message"] = error["message"];
+
+      var mistake = error["sentence"].slice(error["offset"],error["offset"]+error["length"]);
+      errObj["mistake"] = mistake;
+
+      var suggestions = [];
+      for (let i = 0; i < error["replacements"].length && i <= 2; i++) {
+        suggestions.push(error["replacements"][i]["value"]);
+      }
+      errObj["suggestions"] = suggestions;
+
+      errList.push(errObj);
+    });
+    this.setState({
+      hasSubmitted: true,
+      errors: errList
+    });
+    console.log(this.state.errors);
   }
 
   render() {
@@ -75,6 +117,36 @@ class Session extends React.Component {
         </button>
       </li>
     ));
+
+    let feedbackMessage;
+    if (!this.state.hasSubmitted){
+      feedbackMessage = <button id="submit" onClick={this.checkInput}>Check!</button>
+    }
+    else if(this.state.errors.length == 0){
+      feedbackMessage = <>
+        <p id="passed">Voila! Well done</p>
+        <button id="submit" onClick={this.reset}>More Words!</button>
+      </>
+    }
+    else {
+      var errorDisplay = this.state.errors.map(err => (
+        <>
+          <p id="mistakes">{err.message}</p>
+          <p id="fixLine">Mistake: <span id="mistakes">{err.mistake}</span></p>
+          <p id="fixLine"><span>Suggestions: </span> 
+            {err.suggestions.map((e, i) => [
+                i > 0 && ", ",
+                <span id="passed">{e}</span>
+            ])}
+          </p>
+        </>
+      ));
+      feedbackMessage = <>
+        {errorDisplay}
+        <br />
+        <button id="submit" onClick={this.checkInput}>Check!</button>
+      </>
+    }
     return (
     <>
       <h1>Mumbo Jumbo!</h1>
@@ -84,11 +156,11 @@ class Session extends React.Component {
         <textarea value={this.state.userInput} onChange={this.userTyping} spellcheck="false" maxlength="128"/>
       </div>
       <p id="charCount">{this.state.charCount}/128</p>
-      <button id="submit" onClick={this.checkInput}>Submit!</button>
+      <br />
+      {feedbackMessage}
       <hr />
       <span>
         <p className="footer">Created by Edgineer</p>
-        <p className="footer">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
       </span>
     </>
     );
